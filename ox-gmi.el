ab794f91d07b94637a711a98b0c67aed05c7038a
Étienne Deparis <etienne@depar.is>
2020-11-30 18:45:58 +0100

    Do not add caption in code blocks if it’s an empty string

--- ox-gmi.el
+++ ox-gmi.el
@@ -155,7 +155,9 @@ contents according to the specified element."
 INFO is a plist used as a communication channel."
   (let ((language (org-export-data (org-element-property :language block) info))
         (caption (org-export-data (org-element-property :caption block) info)))
-    (setq caption (if caption (format "%s - %s" language caption) language))
+    (setq caption (if (and caption (not (string= "" caption)))
+                      (format "%s - %s" language caption)
+                    language))
     (format "```%s\n%s```\n"
             caption
             (org-remove-indentation
